64317c19e0abbcd10b97847f294ebe5281229351
Kserhi <movie.to.48@gmail.com>
2024-12-03 15:16:10 +0200

    add some old services

---
+++ sql-initializer/src/main/java/com/example/sqlinitializer/SqlInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlInitializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlInitializerApplication.class, args);
+    }
+
+}
