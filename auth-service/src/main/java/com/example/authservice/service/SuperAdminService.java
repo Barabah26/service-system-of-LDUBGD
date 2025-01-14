d65901c8454aca1f536f085d4729abfa0a7e7b83
pavlobarabah <pavlobarabah@gmail.com>
2025-01-14 21:44:54 +0200

    added superAdmin page

---
+++ auth-service/src/main/java/com/example/authservice/service/SuperAdminService.java
@@ -0,0 +1,22 @@
+package com.example.authservice.service;
+
+import com.example.authservice.dto.AdminDto;
+import com.example.authservice.dto.UpdateAdminDto;
+import com.example.authservice.entity.Admin;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface SuperAdminService {
+    Optional<Admin> getByLogin(@NonNull String login);
+
+    AdminDto registerAdmin(AdminDto adminDto);
+
+    List<Admin> getAllAdmins();
+
+    void deleteAdminByLogin(String login);
+
+    UpdateAdminDto updateAdminPassword(String login, UpdateAdminDto userDto);
+}
