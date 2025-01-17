b4c9ac2b58127f6d48c18355923851d6ea5e492f
Kserhi <movie.to.48@gmail.com>
2025-01-17 14:53:59 +0200

    add tread of notification

--- notificationService/src/main/java/com/ldubgd/notificationService/services/NotificationService.java
+++ notificationService/src/main/java/com/ldubgd/notificationService/services/NotificationService.java
@@ -3,6 +3,4 @@ package com.ldubgd.notificationService.services;
 public class NotificationService {
 
 
-
-
 }
---
+++ notificationService/src/main/java/com/ldubgd/notificationService/services/UpdateUserStatusService.java
@@ -0,0 +1,48 @@
+package com.ldubgd.notificationService.services;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class UpdateUserStatusService {
+
+    @Autowired
+    private ScheduledExecutorService scheduler;
+
+    private Runnable task;
+
+    @PostConstruct
+    public void init() {
+        task = () -> {
+            try {
+                sendNotificationAboutStatementStatus();
+            } catch (Exception e) {
+                System.out.println("Помилка відправки повідомлення про статус заявки: " + e.getMessage());
+            }
+        };
+        scheduler.scheduleAtFixedRate(
+                task,
+                0,1,
+                TimeUnit.SECONDS);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        scheduler.shutdown();
+        System.out.println("Зупинено відправку повідомлень про статус заявки");
+    }
+
+    private void sendNotificationAboutStatementStatus() {
+        System.out.println("hhhhhhhhh");
+    }
+
+}
