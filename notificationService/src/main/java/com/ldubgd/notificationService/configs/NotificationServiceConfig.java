4ac3963cde8337f1a865f98bf9ada7302524c93f
Kserhi <movie.to.48@gmail.com>
2025-01-21 22:58:26 +0200

    add http sender

--- notificationService/src/main/java/com/ldubgd/notificationService/configs/NotificationServiceConfig.java
+++ notificationService/src/main/java/com/ldubgd/notificationService/configs/NotificationServiceConfig.java
@@ -4,6 +4,7 @@ import com.ldubgd.utils.CryptoTool;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class NotificationServiceConfig {
@@ -14,5 +15,10 @@ public class NotificationServiceConfig {
     @Bean
     public CryptoTool getCryptoTool(){
         return new CryptoTool(salt);
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
--- notificationService/src/main/java/com/ldubgd/notificationService/services/SendNotificationService.java
+++ notificationService/src/main/java/com/ldubgd/notificationService/services/SendNotificationService.java
@@ -2,15 +2,19 @@ package com.ldubgd.notificationService.services;
 
 import com.ldubgd.components.dao.Statement;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class SendNotificationService {
 
+    private final WebClient.Builder webClientBuilder;
+
+    public SendNotificationService(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
     public void sendNotificationAboutStatementStatus(Statement statement ) {
-        System.out.println(statement.getFullName()+" "+statement.getUser().getEmail());
 
-
-        new RuntimeException("Not implemented yet");
     }
 
 
