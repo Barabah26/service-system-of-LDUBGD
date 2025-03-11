f0bfe1b23e3d51f363da939f0a9b25d03aedb5e1
pavlobarabah <pavlobarabah@gmail.com>
2025-03-11 09:11:42 +0200

    modified vite and nginx files

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
     strictPort: true,
-     watch: {
-       usePolling: true
-     }
+    watch: {
+      usePolling: true
+    }
   },
   build: {
     chunkSizeWarningLimit: 1600,
-    },
+  },
 })
+
 
 
 
