2b50f77fba7bc496be943410dd0f3be9671b08af
pavlobarabah <pavlobarabah@gmail.com>
2025-03-11 12:47:30 +0200

    modified nginx, docker and vite files

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: "/",
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     host: true,
     port: 3000,
@@ -16,7 +16,3 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
   },
 })
-
-
-
-
