bdb286ce2fb9c648786f0c041c845f314bcf3bcf
Kserhi <movie.to.48@gmail.com>
2025-03-18 21:42:02 +0200

    rewrote way of sending email

--- emailService/src/main/java/com/ldubgd/emailService/services/EmailSenderService.java
+++ emailService/src/main/java/com/ldubgd/emailService/services/EmailSenderService.java
@@ -1,22 +1,23 @@
 package com.ldubgd.emailService.services;
 
 import com.ldubgd.components.dao.ForgotPassword;
-import com.ldubgd.components.dao.ForgotPasswordInfo;
 import com.ldubgd.components.dao.Statement;
 import com.ldubgd.emailService.repositories.FileInfoRepository;
-import com.ldubgd.emailService.repositories.ForgotPasswordInfoRepository;
 import com.ldubgd.emailService.repositories.ForgotPasswordRepository;
 import com.ldubgd.emailService.repositories.StatementRepository;
 import com.ldubgd.utils.CryptoTool;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmailSenderService {
+
     @Autowired
     private JavaMailSender mailSender;
 
@@ -35,122 +36,130 @@ public class EmailSenderService {
     @Autowired
     private ForgotPasswordRepository forgotPasswordRepository;
 
+    /**
+     * Метод для надсилання HTML-листа.
+     */
+    private void sendHtmlEmail(String toEmail, String subject, String htmlBody) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        helper.setFrom(emailFrom);
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        // true означає, що вміст повідомлення є HTML
+        helper.setText(htmlBody, true);
+        mailSender.send(mimeMessage);
+    }
 
     @Transactional(rollbackFor = Exception.class)
-    public boolean  sendEmailAboutStatementStatus(Long idOfStatement){
+    public boolean sendEmailAboutStatementStatus(Long idOfStatement) {
 
         Statement statement;
-
         try {
-            statement = statementRepository.findById(idOfStatement).orElseThrow(() -> new RuntimeException("Statement not found"));
-        }  catch (Exception e) {
-            System.err.println("Exception occurred while sending notification: " + e.getMessage());
+            statement = statementRepository.findById(idOfStatement)
+                    .orElseThrow(() -> new RuntimeException("Statement not found"));
+        } catch (Exception e) {
+            System.err.println("Exception occurred while fetching statement: " + e.getMessage());
             return false;
         }
 
-
-        String texOfEmail;
-
-        if(fileInfoRepository.existsByStatementId(statement.getId())){
-            String urlOfFile=new StringBuilder("dovidka.ldubgd.edu.ua/api/file/download?id=").append(cryptoTool.hashOf(idOfStatement)).toString();
-            texOfEmail = generateNotificationMessageWithFile(statement,urlOfFile);
-        }else {
-            texOfEmail = generateNotificationMessageForStatement(statement);
+        String htmlContent;
+        if (fileInfoRepository.existsByStatementId(statement.getId())) {
+            String urlOfFile = new StringBuilder("dovidka.ldubgd.edu.ua/api/file/download?id=")
+                    .append(cryptoTool.hashOf(idOfStatement))
+                    .toString();
+            htmlContent = generateHtmlMessageWithFile(statement, urlOfFile);
+        } else {
+            htmlContent = generateHtmlMessageForStatement(statement);
         }
 
-
         try {
-            sendEmail(statement.getUser().getEmail(), "Сповіщення про статус заявки", texOfEmail);
-        } catch (Exception e) {
-            System.err.println("Exception occurred while sending notification: " + e.getMessage());
+            sendHtmlEmail(statement.getUser().getEmail(), "Сповіщення про статус заявки", htmlContent);
+        } catch (MessagingException e) {
+            System.err.println("Exception occurred while sending email: " + e.getMessage());
             return false;
         }
 
         statement.getStatementInfo().setIsReady(true);
-
         statementRepository.save(statement);
 
         return true;
     }
 
-
-
     @Transactional(rollbackFor = Exception.class)
-    public boolean  sendEmailAboutForgotPasswordStatus(Long idOfStatement){
-
-        ForgotPassword  forgotPassword;
-
+    public boolean sendEmailAboutForgotPasswordStatus(Long idOfForgotPassword) {
+        ForgotPassword forgotPassword;
         try {
-            forgotPassword = forgotPasswordRepository.findById(idOfStatement).orElseThrow(() -> new RuntimeException("Statement not found"));
-        }  catch (Exception e) {
-            System.err.println("Exception occurred while sending notification: " + e.getMessage());
+            forgotPassword = forgotPasswordRepository.findById(idOfForgotPassword)
+                    .orElseThrow(() -> new RuntimeException("ForgotPassword request not found"));
+        } catch (Exception e) {
+            System.err.println("Exception occurred while fetching forgot password request: " + e.getMessage());
             return false;
         }
 
-
-        String texOfEmail = generateNotificationMessageForForgotPassword(forgotPassword);
-
-
+        String htmlContent = generateHtmlMessageForForgotPassword(forgotPassword);
 
         try {
-            sendEmail(forgotPassword.getUser().getEmail(), "Сповіщення про статус заявки", texOfEmail);
-        } catch (Exception e) {
-            System.err.println("Exception occurred while sending notification: " + e.getMessage());
+            sendHtmlEmail(forgotPassword.getUser().getEmail(), "Сповіщення про зміну паролю", htmlContent);
+        } catch (MessagingException e) {
+            System.err.println("Exception occurred while sending email: " + e.getMessage());
             return false;
         }
 
         forgotPassword.getForgotPasswordInfo().setIsReady(true);
-
         forgotPasswordRepository.save(forgotPassword);
         return true;
     }
 
-
-
-    private void sendEmail(String toEmail, String subject, String body){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(emailFrom);
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
-        mailSender.send(message);
-    }
-
-    private String generateNotificationMessageForForgotPassword(ForgotPassword forgotPassword) {
-        StringBuilder messageBuilder = new StringBuilder();
-
-        messageBuilder.append("Ваша заявка: '")
-                .append(forgotPassword.getTypeOfForgotPassword())
-                .append("' готова!\n\n")
-                .append("Деталі заявки:\n")
-                .append("- Логін: ").append(forgotPassword.getLogin())
-                .append("\n")
-                .append("- Пароль: ").append(forgotPassword.getPassword());
-        return messageBuilder.toString();
-    }
-
-
-    private String generateNotificationMessageForStatement(Statement statement) {
-        StringBuilder messageBuilder = new StringBuilder();
-
-        messageBuilder.append("Ваша заявка: '")
+    /**
+     * Генерує HTML повідомлення для заявки без посилання на файл.
+     */
+    private String generateHtmlMessageForStatement(Statement statement) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><body>")
+                .append("<p>Ваша заявка: '")
                 .append(statement.getTypeOfStatement())
-                .append("' готова!\n\n")
-                .append("Деталі заявки:\n")
-                .append("- Факультет: ").append(statement.getFaculty()).append("\n")
-                .append("- Група: ").append(statement.getGroup()).append("\n")
-                .append("- Рік народження: ").append(statement.getYearBirthday()).append("\n")
-                .append("- Контактний телефон: ").append(statement.getPhoneNumber()).append("\n\n");
-        return messageBuilder.toString();
+                .append("' готова!</p>")
+                .append("<p>Деталі заявки:<br>")
+                .append("- Факультет: ").append(statement.getFaculty()).append("<br>")
+                .append("- Група: ").append(statement.getGroup()).append("<br>")
+                .append("- Рік народження: ").append(statement.getYearBirthday()).append("<br>")
+                .append("- Контактний телефон: ").append(statement.getPhoneNumber())
+                .append("</p>")
+                .append("</body></html>");
+        return htmlBuilder.toString();
     }
 
+    /**
+     * Генерує HTML повідомлення для заявки з посиланням на файл.
+     */
+    private String generateHtmlMessageWithFile(Statement statement, String url) {
+        // Генеруємо базове повідомлення
+        String baseHtml = generateHtmlMessageForStatement(statement);
+        // Вставляємо посилання для скачування файлу перед закриваючим тегом </body>
+        int closingBodyIndex = baseHtml.lastIndexOf("</body>");
+        String fileLinkHtml = "<p>Файл заявки: <a href=\"" + url + "\" download>Скачати файл</a></p>";
+        if (closingBodyIndex != -1) {
+            return baseHtml.substring(0, closingBodyIndex) + fileLinkHtml + baseHtml.substring(closingBodyIndex);
+        } else {
+            // Якщо з якоїсь причини не знайдено </body>, просто додаємо посилання в кінець
+            return baseHtml + fileLinkHtml;
+        }
+    }
 
-
-    private String generateNotificationMessageWithFile(Statement statement,String url) {
-
-        return new StringBuilder(generateNotificationMessageForStatement(statement))
-                .append("Файл заявки: ")
-                .append(url).toString();
+    /**
+     * Генерує HTML повідомлення для заявки на відновлення паролю.
+     */
+    private String generateHtmlMessageForForgotPassword(ForgotPassword forgotPassword) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><body>")
+                .append("<p>Ваша заявка: '")
+                .append(forgotPassword.getTypeOfForgotPassword())
+                .append("' готова!</p>")
+                .append("<p>Деталі заявки:<br>")
+                .append("- Логін: ").append(forgotPassword.getLogin()).append("<br>")
+                .append("- Пароль: ").append(forgotPassword.getPassword())
+                .append("</p>")
+                .append("</body></html>");
+        return htmlBuilder.toString();
     }
 }
-
